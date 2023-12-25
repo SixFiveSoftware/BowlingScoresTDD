4778e50a8914671285e61cc510a6135c1062e32f
BJ Miller <bjmiller@skiplist.com>
2023-12-25 10:20:04 -0500

    Initial Commit

---
+++ BowlingScoresTDD/BowlingScoresTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingScoresTDDApp.swift
+//  BowlingScoresTDD
+//
+//  Created by BJ Miller on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BowlingScoresTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
